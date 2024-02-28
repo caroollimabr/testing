3cfd3c9757154508471b05e3ea9426951fca9e6c
caroollimabr <carool.limabr@gmail.com>
2024-02-27 21:34:29 -0300

    estudos com locust

---
+++ locust/second_test/locustfile.py
@@ -0,0 +1,19 @@
+import time
+from locust import HttpUser, task, between
+
+
+class QuickstartUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def hello_world(self):
+        self.client.get("/hello")
+
+    @task(3)  # makes Locust three times more likely to pick view_items than hello_world
+    def view_items(self):
+        for item_id in range(10):
+            self.client.get(f"/item?id={item_id}", name="/item")
+            time.sleep(1)
+
+    def on_start(self):
+        self.client.post("/login", json={"username": "Admin", "password": "admin123"})
